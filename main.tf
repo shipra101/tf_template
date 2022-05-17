f68c3ce397c8b98d72c45d28cf4ce5b5a26020d9
shipra101 <77984471+shipra101@users.noreply.github.com>
2022-05-17 19:01:04 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,40 @@
+terraform {
+    required_providers {
+        ibm = {
+        source = "IBM-Cloud/ibm"
+        version = "~> 1.33.0"
+        }
+    }
+ }
+
+provider "ibm" {
+}
+
+variable "resource_group_id" {
+  description = "ID of the resource group."
+  type        = string
+}
+
+
+resource "ibm_resource_instance" "cos_instance" {
+  name              = "demo-cos"
+  service           = "cloud-object-storage"
+  plan              = "standard"
+  location          = "global"
+  resource_group_id = var.resource_group_id
+  tags = ["t1", "t2"]
+
+}
+
+
+# module "resource_group" {
+  
+#   source = "terraform-ibm-modules/resource-management/ibm//modules/resource-group"
+
+#   provision = var.provision
+#   name      = var.name
+# }
+
+# resource "ibm_resource_group" "resourceGroup" {
+#   name     = "prod"
+# }
