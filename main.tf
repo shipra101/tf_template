bf017e75bf81a467ca9db888df1b05d0a00adfb4
shipra101 <77984471+shipra101@users.noreply.github.com>
2023-03-20 16:14:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ variable "apikey" {
 # }
 
 resource "ibm_cloudant" "cloudant" {
-  name     = "demo2-cloudant"
+  name     = "demo3-cloudant"
   location = "eu-gb"
   plan = var.plan
   resource_group_id = var.resource_group_id
